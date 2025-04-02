1f69c94b5f1fb3687497dd4abf936a4c117e3976
piyush147 <piyushkumar9818@gmail.com>
2025-04-02 15:51:51 +0530

    Edge server or API gateway, which helps in a lot of scenarios regarding security logging routing, caching and many other.

---
+++ MicroserviceApplications/gatewayserver/src/main/java/com/springBoot/gatewayserver/GatewayserverApplication.java
@@ -0,0 +1,42 @@
+package com.springBoot.gatewayserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class GatewayserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayserverApplication.class, args);
+	}
+
+	@Bean
+	public RouteLocator customRouteLocator(RouteLocatorBuilder routeLocatorBuilder){
+		return routeLocatorBuilder.routes()
+				.route(p -> p
+						.path("/banking/accounts/**")
+						.filters(filter ->
+								filter.rewritePath("banking/accounts/?(?<remaining>.*)","/${remaining}")
+						)
+						.uri("lb://ACCOUNTS")
+				)
+				.route(p -> p
+						.path("/banking/cards/**")
+						.filters(filter ->
+								filter.rewritePath("banking/cards/?(?<remaining>.*)","/${remaining}")
+						)
+						.uri("lb://CARDS")
+				)
+				.route(p -> p
+						.path("/banking/loans/**")
+						.filters(filter ->
+								filter.rewritePath("banking/loans/?(?<remaining>.*)","/${remaining}")
+						)
+						.uri("lb://LOANS")
+				)
+				.build();
+	}
+}
