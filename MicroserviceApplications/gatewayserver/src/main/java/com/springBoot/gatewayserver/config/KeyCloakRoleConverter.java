ed0291dd3f76ca3083e9f6291b6e6a0418cc5a27
piyush147 <piyushkumar9818@gmail.com>
2025-04-18 14:21:43 +0530

    configured custom role converter as keycloak does not provide authorities in the jwt from where spring's default JwtGrantedAuthenticationConverter reads it.

---
+++ MicroserviceApplications/gatewayserver/src/main/java/com/springBoot/gatewayserver/config/KeyCloakRoleConverter.java
@@ -0,0 +1,33 @@
+package com.springBoot.gatewayserver.config;
+
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class KeyCloakRoleConverter implements Converter<Jwt, Collection<GrantedAuthority>> {
+
+    @Override
+    public Collection<GrantedAuthority> convert(Jwt jwt) {
+        Map<String, Object> realmAccess = jwt.getClaim("realm_access");
+
+        if (realmAccess == null || realmAccess.isEmpty()) {
+            return Collections.emptyList();
+        }
+
+        Collection<String> roles = (Collection<String>) realmAccess.get("roles");
+
+        System.out.printf(roles.stream().distinct().collect(Collectors.joining()));
+        return roles.stream()
+                .map(role -> "ROLE_"+role)
+                .map(SimpleGrantedAuthority::new)
+                .collect(Collectors.toSet());
+    }
+}
